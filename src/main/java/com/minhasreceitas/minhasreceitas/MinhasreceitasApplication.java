405e2a04076b5924eba190f98cae20684e827567
Judson Cabral de Melo <judsoncabral1@hotmail.com>
2023-09-07 20:02:28 -0300

    chore: initial config

---
+++ src/main/java/com/minhasreceitas/minhasreceitas/MinhasreceitasApplication.java
@@ -0,0 +1,13 @@
+package com.minhasreceitas.minhasreceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasreceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasreceitasApplication.class, args);
+	}
+
+}
